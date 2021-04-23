eb43fe9165e3377fcb518058e06d3eb7377b4583
Muzaffer <aydogdum38@gmail.com>
2021-04-23 18:09:12 +0300

    added category validation

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router()
 const categoryController = require("../controllers/categoryController")
+const validations = require("../middleware/validationMiddleware")
 
 // api/categories
 
 
-router.post("/addCategory", categoryController.addCategory);
+router.post("/addCategory", validations.categoryValidation , categoryController.addCategory);
 
 
 router.get("/getCategory/:id", categoryController.getCategory)
 
-router.post("/updateCategory", categoryController.updateCategory)
+router.post("/updateCategory", validations.categoryValidation, categoryController.updateCategory)
 
 router.get("/deleteCategory/:id", categoryController.deleteCategory)
 
