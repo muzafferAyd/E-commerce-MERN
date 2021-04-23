64ee345755744ccb68d7b2222067e02a603e01a1
Muzaffer <aydogdum38@gmail.com>
2021-04-23 15:13:22 +0300

    addded product-category routers

---
+++ routers/categoryRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router()
+const categoryController = require("../controllers/categoryController")
+
+// api/categories
+
+
+router.post("/addCategory", categoryController.addCategory);
+
+
+router.get("/getCategory/:id", categoryController.getCategory)
+
+router.post("/updateCategory", categoryController.updateCategory)
+
+router.get("/deleteCategory/:id", categoryController.deleteCategory)
+
+
+router.get("/" , categoryController.getCategories)
---
+++ routers/productRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+const productController = require("../controllers/productController")
+
+// api/products/
+
+
+router.post("/addProduct", productController.addProduct);
+
+router.get("/getProduct/:id", productController.getProduct);
+
+router.post("/updateProduct/:id", productController.updateProduct);
+
+router.get("/deleteProduct/:id", productController.deleteProduct);
+
+router.get("/", productController.getProducts)
